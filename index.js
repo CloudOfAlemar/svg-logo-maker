abc1270d0516879a8c39726ffe18ff3be56674e2
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-06-04 13:48:00 -0700

    1). Created the promptSVGShape function to receive an SVG shape input.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-const { promptSVGText, promptSVGTextColor } = require( "./lib/inquirerPrompts" );
+const { promptSVGText, promptSVGTextColor, promptSVGShape } = require( "./lib/inquirerPrompts" );
 
 promptSVGText()
   .then( answers => {
     return promptSVGTextColor( answers );
   } )
   .then( answers => {
+    return promptSVGShape( answers );
+  } )
+  .then( answers => {
     console.log( answers );
   } );
+
