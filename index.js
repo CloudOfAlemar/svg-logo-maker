254ab116ed2bbaa57566b6a0696f6679ddaf15cd
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-06-11 19:12:28 -0700

    Added comments on all files.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
+/*
+  Require Modules
+*/
 const fs = require( "fs/promises" );
 const { promptSVGText, promptSVGTextColor, promptSVGShape, promptSVGBgColor } = require( "./lib/inquirerPrompts" );
 const getSVG = require( "./lib/getSVG" );
 
+/*
+  Get User Info
+    1). Prompt for the SVG Text
+    2). Followed by prompting for the SVG Text Color
+    3). Then prompt for a shape
+    4). Prompt for SVG Background Color
+    5). Get the SVG code and create the logo.svg file
+        using fs.writeFile
+    6). Finally, console.log "Generated logo.svg"
+*/
 promptSVGText()
   .then( answers => {
     return promptSVGTextColor( answers );
